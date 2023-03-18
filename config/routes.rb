5a39f10d0c9dd464a0805272e557f8303e9d0e38
Sandra Vilder <sandra.vilder@gmail.com>
2023-03-18 10:04:02 +0100

    c/p routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope '(:locale)', locale: /fr|en/ do
+    root to: 'pages#home'
+    get 'about', to: 'pages#about', as: :about
+    get 'films', to: 'pages#films', as: :films
+    resources :texts, only: [:show]
+    resources :works, only: [:index, :show]
+  end
 end
