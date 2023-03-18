e011e29506f38301c4895b67661e665a0c0867c4
Sandra Vilder <sandra.vilder@gmail.com>
2023-03-18 09:59:18 +0100

    c/p DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,112 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_18_083646) do
+ActiveRecord::Schema[7.0].define(version: 2020_07_27_134059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.string "author_type"
+    t.bigint "author_id"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "bibliographies", force: :cascade do |t|
+    t.string "author"
+    t.string "title"
+    t.integer "year"
+    t.string "month"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "exhibitions", force: :cascade do |t|
+    t.string "title"
+    t.string "place"
+    t.string "city"
+    t.string "category"
+    t.integer "year"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.string "associates"
+  end
+
+  create_table "texts", force: :cascade do |t|
+    t.string "title_fr"
+    t.string "title_en"
+    t.string "author"
+    t.text "content_fr"
+    t.text "content_en"
+    t.integer "year"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "works", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "dimensions"
+    t.integer "year"
+    t.string "image"
+    t.string "video_key"
+    t.string "display_option"
+    t.string "collection"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require_relative 'seed/work_seed.rb'
+require_relative 'seed/text_seed.rb'
+require_relative 'seed/exhibition_seed.rb'
+require_relative 'seed/bibliography_seed.rb'
+require 'csv'
+require 'open-uri'
+
+MODELS = %w[Work Text Exhibition Bibliography]
+
+strip = lambda {|f| f ? f.strip : nil}
+
+MODELS.each do |model|
+  puts "#{model.pluralize} cleaning"
+  model.constantize.destroy_all
+  puts "Generating #{model} data"
+
+  csv_text = File.read(Rails.root.join('lib',"seeds/#{model.underscore.pluralize}.csv"))
+  csv = CSV.parse(csv_text, headers: true, converters: strip)
+
+  "#{model}Seed".constantize.new(csv).call
+  
+  puts "#{model.constantize.count} #{model.pluralize} has been seeded"
+end
