3b6c131ce327df0cd04793a1419ec35b87631439
Sandra Vilder <sandra.vilder@gmail.com>
2023-03-27 16:32:20 +0200

    js console log tests

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  initialize() {
+    console.log("salut les nazes")
+  }
   connect() {
+    console.log("salut les nazes")
     this.element.textContent = "Hello World!"
+    console.log("Hello, Stimulus!", this.element)
   }
 }
